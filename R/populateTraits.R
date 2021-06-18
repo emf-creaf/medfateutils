5b66af0e85581714465e737cee9018005b3d1b20
Miquel De Cáceres <miquelcaceres@gmail.com>
2021-06-18 13:58:57 +0200

    populate traits

--- R/populateTraits.R
+++ R/populateTraits.R
@@ -4,8 +4,8 @@
 #'
 #' @param SpParams A data frame of medfate species parameters to be populated.
 #' @param trait_table A data frame with species traits in columns and row.names corresponding to taxonomic entities.
-#' @param trait_mapping A named list specifying which trait data column should used to populate each medfate param. Elements are data base columns and names are medfate params.
-#' @param transformation_functions A named list of scalar functions for traits needing transformation of units or scaling.
+#' @param trait_mapping A named string vector specifying which trait data column should used to populate each medfate param. Elements are data base columns and names are medfate params.
+#' @param transformation_functions A named list of scalar functions for traits needing transformation of units or scaling. Names are medfate params.
 #' @param erase_previous A boolean flag to indicate that values should be set to NA before populating with data
 #'
 #' @return A modified data frame of medfate species parameters
@@ -25,8 +25,8 @@ populateTraits<-function(SpParams,
 
   if(erase_previous) SpParams[,trait_params] = NA
 
-  for(j in 1:trait_mapping) {
-    trait <- trait_mapping[j]
+  for(j in 1:length(trait_mapping)) {
+    trait <- trait_mapping[[j]]
     param <- trait_params[j]
     nmis <- 0
     nsp <- 0
@@ -60,6 +60,11 @@ populateTraits<-function(SpParams,
       }
       if(!is.na(trait_row)) {
         SpParams[i, param] <- trait_table[trait_row, trait]
+        if(param %in% names(transformation_functions)) {
+          f <- transformation_functions[[param]]
+          p <- list(SpParams[i,param])
+          SpParams[i, param] <- do.call(f, p)
+        }
       } else {
         nmis <- nmis +1
       }
@@ -67,7 +72,7 @@ populateTraits<-function(SpParams,
     message(paste0("Mapping [",trait_mapping[j]," -> ",trait_params[j], "] species: ", nsp, " genus: ", ngen," family: ",nfam, " order:", norder," group: ", ngroup))
     if(nmis>0) message(paste0(nmis,
                               " missing trait values (out of ",
-                              ntree,
+                              nrow(SpParams),
                               " tree species) after populating with input data.\n"))
   }
   return(SpParams)
